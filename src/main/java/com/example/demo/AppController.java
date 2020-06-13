5b095da520c76035a7ce33900b9ad58097eb4a58
Robb <rubistan@gmail.com>
2020-06-13 22:32:22 +0300

    Guinness version 1

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -54,24 +54,19 @@ public class AppController {
 
     private String doQuery(String keyword) throws IOException {
         OkHttpClient client = new OkHttpClient();
-        System.out.println("query for " + keyword);
         Request request = new Request.Builder()
-                .url("https://www.amazon.com/s?i=aps&k="+ keyword + "&ref=nb_sb_noss&url=search-alias%3Daps")
+                .url("https://www.guinnessworldrecords.com/search/results?Header=&Term="+ keyword + "&Page=1&Type=all&Max=20&FacetType=&FacetContentType=&FacetCategories=&FacetLocation=&FacetIndustry=&FacetPurpose=&FacetPubDate=&Partial=_Results&Filter1=&Filter2=&Filter3=&AudienceType=&AutoSearch=false")
                 .method("GET", null)
                 .addHeader("Connection", "keep-alive")
-                .addHeader("rtt", "200")
-                .addHeader("downlink", "1.5")
-                .addHeader("ect", "4g")
-                .addHeader("Upgrade-Insecure-Requests", "1")
-                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36")
-                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .addHeader("Accept", "*/*")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36")
+                .addHeader("X-Requested-With", "XMLHttpRequest")
                 .addHeader("Sec-Fetch-Site", "same-origin")
-                .addHeader("Sec-Fetch-Mode", "navigate")
-                .addHeader("Sec-Fetch-User", "?1")
-                .addHeader("Sec-Fetch-Dest", "document")
-                .addHeader("Referer", "https://www.amazon.com/s?k=ipad&ref=nb_sb_noss_1")
+                .addHeader("Sec-Fetch-Mode", "cors")
+                .addHeader("Sec-Fetch-Dest", "empty")
+                .addHeader("Referer", "https://www.guinnessworldrecords.com/search?term=mountaneering&page=1&type=all&max=20&partial=_Results&")
                 .addHeader("Accept-Language", "en-US,en;q=0.9,he;q=0.8")
-                .addHeader("Cookie", "s_fid=4736780042B9999F-0390A4B8EB8FDFC0; aws-priv=eyJ2IjoxLCJldSI6MCwic3QiOjB9; aws-target-static-id=1589374591307-262665; aws-target-data=%7B%22support%22%3A%221%22%7D; aws-target-visitor-id=1589374590616-764874.37_0; aws-ubid-main=858-8232702-6823551; aws-account-alias=itamar-medical; regStatus=registered; session-id=145-0020258-6872618; session-id-time=2082787201l; i18n-prefs=USD; sp-cdn=\"L5Z9:IL\"; ubid-main=134-9698648-9078067; x-wl-uid=1nn3ZmMEoiFFkqReu+paSxZ2AVbTRTSztHzg8kDKtrFk9TzkEunozuSJ0SJg/xP2H1Yuf8MOoklo=; s_vn=1620907567092%26vn%3D9; s_dslv=1590578215532; s_nr=1590578215536-Repeat; aws-userInfo=%7B%22arn%22%3A%22arn%3Aaws%3Aiam%3A%3A831311657046%3Auser%2FNiv%22%2C%22alias%22%3A%22itamar-medical%22%2C%22username%22%3A%22Niv%22%2C%22keybase%22%3A%22%22%2C%22issuer%22%3A%22http%3A%2F%2Fsignin.aws.amazon.com%2Fsignin%22%2C%22signinType%22%3A%22PUBLIC%22%7D; skin=noskin; session-token=9ZO5419FUtyUT1dlaoxH0MUDQDHlbQ9VGwX6TpAGVr2vqcbqlbqMkxSLUmRGZPcKNZsLTPuPINUMWJD/mS71j8x0OF78/cd396ookiwtdwe4zTpecqC9a0Inq3q7Zm5mP/GMYtUh4S50gVI+yx6+1MYDIs0EStAj1UfG28aFVHu13bmBxqjDaNvQxlBmWpkE; csm-hit=tb:M10EKWPRD6DT5MQVG340+s-M10EKWPRD6DT5MQVG340|1591167178737&t:1591167178737&adb:adblk_no")
+                .addHeader("Cookie", "_fbp=fb.1.1592073934021.674382307; __SessionId=jcju0yefxtnjwhbigvzegya4; _ga=GA1.2.849953986.1592073937; _gid=GA1.2.1793213006.1592073937; visitor_id175512=239304190; visitor_id175512-hash=fc55262ea8af0ce772d716e2b92b51f0c4e74c42f138a097cd79ee92cfec09266558e803afe5b7b31594bb7e6f39f08623683f24; cookiesAllowed=true; _uetsid=cda46542-02df-4dcf-1ed6-fa537ea0c63d; _uetvid=74ba0022-040e-f14d-163f-9629c8119af0; AWSALB=dgLPbVYswGgHMadNOh4jPKGuiMhrbTkhSwkj9SP0YF5lhM6lhovFJYWMzXziXoxgmxX0mhp/XHPgT4ByH6TmOC79AUdFxuVBDxHLIQlGgN/BmQ2NOZf0Wg6+2s6z; AWSALBCORS=dgLPbVYswGgHMadNOh4jPKGuiMhrbTkhSwkj9SP0YF5lhM6lhovFJYWMzXziXoxgmxX0mhp/XHPgT4ByH6TmOC79AUdFxuVBDxHLIQlGgN/BmQ2NOZf0Wg6+2s6z; AWSALB=V6uNo5xjis5i/OdoV3nOF45amAPnFJQHN9w5ZdiebiFGWvHekMZQCw4wuVbPzmc8kh9PdvIniHPDKVBJf81CCuAXF/gm+Z+qaN8N8y2sR8u6U7YacEm5thke9L4Y; AWSALBCORS=V6uNo5xjis5i/OdoV3nOF45amAPnFJQHN9w5ZdiebiFGWvHekMZQCw4wuVbPzmc8kh9PdvIniHPDKVBJf81CCuAXF/gm+Z+qaN8N8y2sR8u6U7YacEm5thke9L4Y")
                 .build();
         Response response = client.newCall(request).execute();
         return response.body().string();
@@ -79,16 +74,15 @@ public class AppController {
     private String process(String keyword) throws IOException {
         String res = "";
         String text = doQuery(keyword);
-        Pattern name = Pattern.compile("span class=\"a-size-medium a-color-base a-text-normal\" dir=\"auto\">([A-Za-z0-9\\(\\)\\- ]+)</span>                                </a>    </h2>            </div>");
+        System.out.println(text.replace("\n",""));
+        Pattern title = Pattern.compile("<a href=\"(/world-records/[0-9a-z\\-]+)\">(.*)</a></h3>        </header>");
         Pattern price = Pattern.compile("<span class=\"a-offscreen\">([0-9.$]+)</span><span aria-hidden=\"true\"><span class=\"a-price-symbol\">");
-        String[] prods = (text.replace("\n","").split("</div></div>        </div>      </div></div>    </div>  </div></div></span>"));
-        for (String  p : prods) {
-            //System.out.println(p);
-            Matcher m = name.matcher(p);
-            Matcher pr = price.matcher(p);
-            if (m.find() && pr.find()) {
-                System.out.println(m.group(1) + " price: " +  pr.group(1));
-                res += m.group(1) + " price: " +  pr.group(1) ;
+        String[] articles = (text.replace("\n","").split("</article><article"));
+        for (String  a : articles) {
+            System.out.println(a);
+            Matcher m = title.matcher(a);
+            if (m.find()) {
+                res += m.group(1) + "->" + m.group(2);
             }
 
         }
